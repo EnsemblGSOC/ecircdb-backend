a36473f001f14e092d20d6e16b62658502d44a35
Praduman Goyal <goyal.praduman611@gmail.com>
2019-07-04 22:01:50 +0530

    Add tariling slash in the urls

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,9 @@ from core import views
 
 urlpatterns = [
     path('species/', views.SpeciesList.as_view()),
-    path('species/<int:pk>', views.SpeciesDetail.as_view()),
-    path('species/samples/<int:species>', views.SampleList.as_view()),
-    path('samples/<int:pk>', views.SampleDetail.as_view()),
+    path('species/<int:pk>/', views.SpeciesDetail.as_view()),
+    path('species/samples/<int:species>/', views.SampleList.as_view()),
+    path('samples/<int:pk>/', views.SampleDetail.as_view()),
     path('species_view_stats/<int:species_id>/<int:assembly_id>/',
          views.species_view_stats),
     path('sample_view_stats/<int:species_id>/<int:assembly_id>/<int:sample_id>/',
