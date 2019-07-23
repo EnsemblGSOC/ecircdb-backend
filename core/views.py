24deaf36ea5e293d076bce22a143b267be8c860a
pradumangoyal <goyal.praduman611@gmail.com>
2019-07-23 05:39:33 +0530

    Add some plots to the speciesviewstats

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from django.db import connection
+import pandas as pd
 from rest_framework import filters, generics, permissions, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -118,11 +120,67 @@ def species_view_stats(request, species_id, assembly_id):
         analysis_id__in=analysis_id_list).distinct()
     count_backsplice_junctions = backsplice_junctions.count()
 
+    # Graph for circRNA for each locus
+    circRNA_per_locus_query = 'select locus_id_id, count(*) as count from core_backsplicejunction where locus_id_id in (select locus_id from core_locus where assembly_id_id={}) group by locus_id_id;'.format(
+        assembly.assembly_id)
+    circRNA_per_locus_dataframe = pd.read_sql_query(
+        circRNA_per_locus_query, connection)
+    circRNA_per_locus = {
+        "locus_id": circRNA_per_locus_dataframe["locus_id_id"].apply(str).to_list(),
+        "count": circRNA_per_locus_dataframe["count"].to_list()
+    }
+
+    # Graph for circRNA vs linear transcripts for each locus
+    locus_nexons_query = 'select locus_id, nexons from core_locus where assembly_id_id={};'.format(
+        assembly.assembly_id)
+    cj_query = 'select locus_id_id, count(*) as count from core_canonicaljunction where locus_id_id in (select locus_id from core_locus where assembly_id_id={}) group by locus_id_id;'.format(
+        assembly.assembly_id)
+    locus_nexons_df = pd.read_sql_query(locus_nexons_query, connection)
+    cj_df = pd.read_sql_query(cj_query, connection)
+    locus_cj_df = pd.merge(locus_nexons_df, cj_df,
+                           left_on='locus_id', right_on='locus_id_id', how='outer').fillna(0, downcast='infer')
+    locus_cj_bj_df = pd.merge(locus_cj_df, circRNA_per_locus_dataframe,
+                              left_on='locus_id', right_on='locus_id_id', how='outer', suffixes=('_cj', '_bj')).fillna(0, downcast='infer')
+    circrna_vs_lt_per_locus = {
+        "locus_id": locus_cj_bj_df["locus_id"].to_list(),
+        "count_cj": locus_cj_bj_df["count_cj"].to_list(),
+        "count_bj": locus_cj_bj_df["count_bj"].to_list(),
+        "nexons": locus_cj_bj_df["nexons"].to_list(),
+        "text": "Locus " + locus_cj_bj_df['locus_id'].map(str) + "<br>Exons: " + locus_cj_bj_df['nexons'].map(str)
+    }
+
+    # Graph for tissue TPM box plot
+    tpm_query = 'select tpm, analysis_id_id from core_locusexpression where analysis_id_id in (select analysis_id from core_analysis where assembly_id_id={});'.format(
+        assembly.assembly_id)
+    sample_query = 'select source, sample_id from core_sample where sample_id in (select sample_id_id from core_analysis where assembly_id_id={});'.format(
+        assembly.assembly_id)
+    sample_analysis_query = 'select analysis_id, sample_id_id from core_analysis where assembly_id_id={};'.format(
+        assembly.assembly_id)
+    tpm_df = pd.read_sql_query(tpm_query, connection)
+    sample_df = pd.read_sql_query(sample_query, connection)
+    sample_analysis_df = pd.read_sql_query(sample_analysis_query, connection)
+    tpm_sample_analysis_df = pd.merge(
+        tpm_df, sample_analysis_df, left_on='analysis_id_id', right_on='analysis_id')
+    tpm_sample_merged_df = pd.merge(
+        tpm_sample_analysis_df, sample_df, left_on='sample_id_id', right_on='sample_id')
+    tpm_sample_grouped_df = tpm_sample_merged_df.groupby(['source'])[
+        'tpm'].apply(list)
+    tpm_grouped_list = tpm_sample_grouped_df.to_dict()
+    source_list = tpm_sample_grouped_df.keys().to_list()
+    tpm_tissue_boxplot = {
+        'tpm_grouped': tpm_grouped_list,
+        'tissue_list': source_list
+    }
+
     data = {'species': species.scientific_name,
             'assembly': assembly.assembly_name,
             'count_total_samples': count_total_samples,
             'count_distinct_tissues': count_distinct_tissues,
-            'count_backsplice_junctions': count_backsplice_junctions}
+            'count_backsplice_junctions': count_backsplice_junctions,
+            'circRNA_per_locus': circRNA_per_locus,
+            'circrna_vs_lt_per_locus': circrna_vs_lt_per_locus,
+            'tpm_tissue_boxplot': tpm_tissue_boxplot
+            }
 
     return Response(data=data, status=status.HTTP_200_OK)
 
